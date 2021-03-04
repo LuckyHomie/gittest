e45dfee607b3e9eaa6a7c93cdd133a969e00bbb4
Łukasz Szuligowski <lszuligowski@gmail.com>
2021-03-04 16:46:48 +0100

    zmiany przy SourceTree

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+int Policz(int a, int b)
+{
+    return (a+b)*(a-b);
+}
+
 int main() {
-    printf("Hello, World!\n");
+    printf("W trzecim branchu; (2+3)*(2-3)=%d\n",Policz(2,3));
     return 0;
 }
